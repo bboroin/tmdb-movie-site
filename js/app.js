ed6c58eaf30b5f737eed31a11aba82c56fbc2236
로인 <151013952+bboroin@users.noreply.github.com>
2025-09-26 18:21:48 +0900

    feat: TMDB API 연동 및 인기 영화 데이터 출력 #1

---
+++ js/app.js
@@ -0,0 +1,23 @@
+const options = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization: `Bearer ${API_KEY}`,
+  },
+};
+
+async function getPopularMovie() {
+  try {
+    const response = await fetch(
+      "https://api.themoviedb.org/3/movie/popular?language=ko-KR&page=1",
+      options
+    );
+    const data = await response.json();
+    data.results.forEach((movie) => {
+      console.log(movie.title);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+getPopularMovie();
