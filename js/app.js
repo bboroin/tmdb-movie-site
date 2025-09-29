e44b353934c8885860eccd7e08b851cc4b4e8b71
로인 <151013952+bboroin@users.noreply.github.com>
2025-09-29 18:15:29 +0900

    feat: 영화 카드 리스트 UI 구현 #2

--- js/app.js
+++ js/app.js
@@ -11,12 +11,26 @@ const options = {
 async function getPopularMovie() {
   try {
     const response = await fetch(
-      "https://api.themoviedb.org/3/movie/popular?language=ko-KR&page=1",
+      "https://api.themoviedb.org/3/movie/popular?language=en-US&page=1",
       options
     );
     const data = await response.json();
+
+    const cardList = document.getElementById("card-list");
+
     data.results.forEach((movie) => {
-      console.log(movie.title);
+      const card = document.createElement("div");
+      card.classList.add("card");
+
+      card.innerHTML = `
+        <div class="card-average">⭐${movie.vote_average}</div>
+        <div class="card-img">
+          <img src="https://image.tmdb.org/t/p/w500/${movie.poster_path}" alt="${movie.title}" />
+        </div>
+        <div class="card-title">${movie.title}</div>
+        <div class="card-description">${movie.overview}</div>
+      `;
+      cardList.appendChild(card);
     });
   } catch (error) {
     console.log(error);
